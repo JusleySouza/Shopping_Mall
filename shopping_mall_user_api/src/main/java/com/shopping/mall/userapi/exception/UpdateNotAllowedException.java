efda8e4561eb15c6ce904c48835d0e7eb4cb0257
Jusley Souza <jusleysouza31@gmail.com>
2024-05-26 23:49:23 -0300

    Creating and implementing the exception class for updates that cannot be made

---
+++ shopping_mall_user_api/src/main/java/com/shopping/mall/userapi/exception/UpdateNotAllowedException.java
@@ -0,0 +1,13 @@
+package com.shopping.mall.userapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class UpdateNotAllowedException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public UpdateNotAllowedException(String exception) {
+		super(exception);
+	}
+}
