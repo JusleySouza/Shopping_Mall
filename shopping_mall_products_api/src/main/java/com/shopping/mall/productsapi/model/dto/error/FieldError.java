f23d8ad2047da1b8b9177ba6157b0ef7c4fdbbd2
Jusley Souza <jusleysouza31@gmail.com>
2024-05-27 11:33:50 -0300

    Creation and implementation of error response standardization classes

---
+++ shopping_mall_products_api/src/main/java/com/shopping/mall/productsapi/model/dto/error/FieldError.java
@@ -0,0 +1,19 @@
+package com.shopping.mall.productsapi.model.dto.error;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Generated
+public class FieldError {
+	private String field;
+	private String message;
+	
+	public FieldError(String field, String message) {
+		this.field = field;
+		this.message = message;
+	}
+	
+}
