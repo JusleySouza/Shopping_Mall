8416aa5ff33f0f59adc5a70a427766c795b5910b
Jusley Souza <jusleysouza31@gmail.com>
2024-05-27 14:42:07 -0300

    Creating and implementing the exception class

---
+++ shopping_mall_products_api/src/main/java/com/shopping/mall/productsapi/exception/ExceptionResponse.java
@@ -0,0 +1,29 @@
+package com.shopping.mall.productsapi.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Generated;
+import lombok.Getter;
+
+@Getter
+@Generated
+public class ExceptionResponse implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected Date timestamp;
+	protected String message;
+	protected String details;
+	
+	public ExceptionResponse() {}
+	
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+}
+
+
