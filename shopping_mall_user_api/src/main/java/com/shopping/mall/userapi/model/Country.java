c83f2c76a63ac84b7052d2e764cec644b6ad6790
Jusley Souza <jusleysouza31@gmail.com>
2024-05-26 22:09:40 -0300

    Creation and implementation of the Country model

---
+++ shopping_mall_user_api/src/main/java/com/shopping/mall/userapi/model/Country.java
@@ -0,0 +1,45 @@
+package com.shopping.mall.userapi.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.shopping.mall.userapi.constants.Conf;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false)
+	private UUID id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String code;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate created;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate changed;
+
+}
