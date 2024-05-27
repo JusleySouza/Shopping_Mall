b20bead152f84af651d4f06688678f900cc31a4d
Jusley Souza <jusleysouza31@gmail.com>
2024-05-27 11:42:12 -0300

    Creation and implementation of the success message configuration class

---
+++ shopping_mall_products_api/src/main/java/com/shopping/mall/productsapi/constants/Messages.java
@@ -0,0 +1,13 @@
+package com.shopping.mall.productsapi.constants;
+
+import lombok.Generated;
+
+@Generated
+public class Messages {
+
+	public static final String created = "Successfully created";
+	
+	public static final String updated = "Successfully updated";
+	
+	public static final String deleted = "Successfully deleted";
+}
