a51045df02fd684c5da222a064d69809410f8242
Jusley Souza <jusleysouza31@gmail.com>
2024-05-27 11:26:56 -0300

    Creating and implementing the ResponseCategoryDTO class

---
+++ shopping_mall_products_api/src/main/java/com/shopping/mall/productsapi/model/dto/ResponseCategoryDTO.java
@@ -0,0 +1,23 @@
+package com.shopping.mall.productsapi.model.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseCategoryDTO {
+
+	private UUID id;
+	private String nameCategory;
+	
+}
