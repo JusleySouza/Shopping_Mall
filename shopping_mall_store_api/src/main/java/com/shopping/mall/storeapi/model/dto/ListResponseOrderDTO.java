e8c728cb33d2ab411f70358a200fdf47149b197a
Jusley Souza <jusleysouza31@gmail.com>
2024-05-28 16:40:07 -0300

    Creation and implementation of the ListResponseOrderDTO class

---
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/ListResponseOrderDTO.java
@@ -0,0 +1,22 @@
+package com.shopping.mall.storeapi.model.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListResponseOrderDTO {
+	
+	public List<ResponseOrderDTO> order;
+
+}
