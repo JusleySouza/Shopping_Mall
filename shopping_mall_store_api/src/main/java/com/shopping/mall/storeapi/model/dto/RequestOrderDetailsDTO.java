68350e228a3b94555cbccbb2233d8ebe847ea0f3
Jusley Souza <jusleysouza31@gmail.com>
2024-05-28 16:37:29 -0300

    Creation and implementation of the RequestOrderDetailsDTO class

---
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/RequestOrderDetailsDTO.java
@@ -0,0 +1,23 @@
+package com.shopping.mall.storeapi.model.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestOrderDetailsDTO {
+	
+	private UUID idProduct;
+	private int amount;
+
+}
