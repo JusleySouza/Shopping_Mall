a04ffdbdeda1088a471af5971eb2f19e8aaf5836
Jusley Souza <jusleysouza31@gmail.com>
2024-05-28 16:51:46 -0300

    Creation and implementation of the CountryDTO class

---
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/user/CountryDTO.java
@@ -0,0 +1,21 @@
+package com.shopping.mall.storeapi.model.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountryDTO {
+	
+	private String name;
+	private String code;
+
+}
