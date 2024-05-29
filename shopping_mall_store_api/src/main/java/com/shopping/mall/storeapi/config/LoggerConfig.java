0cae8aa89a39036a757f08b3b69c0fbbf279a31a
Jusley Souza <jusleysouza31@gmail.com>
2024-05-29 14:24:57 -0300

    Creation of the log settings class

---
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/config/LoggerConfig.java
@@ -0,0 +1,15 @@
+package com.shopping.mall.storeapi.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.shopping.mall.storeapi.services.implement.OrderServicesImplement;
+
+import lombok.Generated;
+
+@Generated
+public class LoggerConfig {
+	
+	public static final Logger LOGGER_ORDER = LoggerFactory.getLogger(OrderServicesImplement.class);
+
+}
