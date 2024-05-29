c14c20b3be326c8c40f6822c5c43ee0927929c0f
Jusley Souza <jusleysouza31@gmail.com>
2024-05-29 13:53:09 -0300

    Creation oh the OrderStatus enum

---
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/enums/OrderStatus.java
@@ -0,0 +1,14 @@
+package com.shopping.mall.storeapi.enums;
+
+import lombok.Generated;
+
+@Generated
+public enum OrderStatus {
+
+	AGPAGAMENTO, 
+	EFETUADO,
+	PROCESSADO, 
+	CONCLUIDO, 
+	CANCELADO;
+
+}
