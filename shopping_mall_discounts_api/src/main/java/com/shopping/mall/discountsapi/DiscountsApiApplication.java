903ef0d60a48e59e20af05396bf3d1bcf0c58b81
Jusley Souza <jusleysouza31@gmail.com>
2024-05-30 19:54:18 -0300

    Main class

---
+++ shopping_mall_discounts_api/src/main/java/com/shopping/mall/discountsapi/DiscountsApiApplication.java
@@ -0,0 +1,16 @@
+package com.shopping.mall.discountsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Generated;
+
+@Generated
+@SpringBootApplication
+public class DiscountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountsApiApplication.class, args);
+	}
+
+}
