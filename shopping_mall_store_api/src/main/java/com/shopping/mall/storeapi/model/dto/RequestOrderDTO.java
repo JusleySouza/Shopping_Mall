f78816917980678e6ade8d63c15b385d83d37e73
Jusley Souza <jusleysouza31@gmail.com>
2024-06-25 21:21:27 -0300

    Adding swagger examples

--- shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/RequestOrderDTO.java
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/RequestOrderDTO.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +21,7 @@ import lombok.ToString;
 @NoArgsConstructor
 public class RequestOrderDTO {
 	
+	@ApiModelProperty(notes = "Id user", example = "000acde0-4d72-4049-9705-0eb5ce87e2fa", required = true)
 	private UUID idUser;
 	@JsonProperty("orderDetails")
 	private List<RequestOrderDetailsDTO> orderDetails;
--- shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/RequestOrderDetailsDTO.java
+++ shopping_mall_store_api/src/main/java/com/shopping/mall/storeapi/model/dto/RequestOrderDetailsDTO.java
@@ -2,6 +2,7 @@ package com.shopping.mall.storeapi.model.dto;
 
 import java.util.UUID;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,7 +18,9 @@ import lombok.ToString;
 @NoArgsConstructor
 public class RequestOrderDetailsDTO {
 	
+	@ApiModelProperty(notes = "Id product", example = "000acde0-4d72-4049-9705-0eb5ce87e2fa", required = true)
 	private UUID idProduct;
+	@ApiModelProperty(notes = "Amount product", example = "5", required = true)
 	private int amount;
 
 }
