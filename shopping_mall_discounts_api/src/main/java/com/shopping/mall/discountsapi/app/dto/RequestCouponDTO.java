c12022d1a5eecdee900128a0061fdadc0d26a94b
Jusley Souza <jusleysouza31@gmail.com>
2024-06-25 21:02:34 -0300

    Adding swagger examples

--- shopping_mall_discounts_api/src/main/java/com/shopping/mall/discountsapi/app/dto/RequestCouponDTO.java
+++ shopping_mall_discounts_api/src/main/java/com/shopping/mall/discountsapi/app/dto/RequestCouponDTO.java
@@ -9,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.shopping.mall.discountsapi.cross.constants.Conf;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,13 +26,19 @@ import lombok.ToString;
 public class RequestCouponDTO {
 	
 	@NotEmpty(message = "{code.not.empty}")
+	@ApiModelProperty(notes = "Code coupon", example = "30OFF", required = true)
 	private String code;
 	@NotNull(message = "{percentage.not.null}")
+	@ApiModelProperty(notes = "Percentage coupon", example = "0.1", required = true)
 	private Double percentage;
+	@ApiModelProperty(notes = "Minimum quantity products", example = "2", required = true)
 	private int minQuantityProducts;
+	@ApiModelProperty(notes = "Minimum order value", example = "300.00", required = true)
 	private Double minOrderValue;
+	@ApiModelProperty(notes = "Maximum discount", example = "80.00", required = true)
 	private Double maxDiscount;
 	@DateTimeFormat(pattern = Conf.dateFormat)
+	@ApiModelProperty(notes = "Expiration date", example = "2024-09-30", required = true)
 	private LocalDate expiration;
 
 }
--- shopping_mall_discounts_api/src/main/java/com/shopping/mall/discountsapi/app/dto/RequestDiscountDTO.java
+++ shopping_mall_discounts_api/src/main/java/com/shopping/mall/discountsapi/app/dto/RequestDiscountDTO.java
@@ -12,6 +12,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import com.shopping.mall.discountsapi.app.enums.TypeDiscount;
 import com.shopping.mall.discountsapi.cross.constants.Conf;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,12 +30,16 @@ public class RequestDiscountDTO {
 	
 	@NotNull(message = "{typeDiscount.not.null}")
 	@Enumerated(EnumType.STRING)
+	@ApiModelProperty(notes = "Type discount", example = "CAT", required = true)
 	private TypeDiscount typeDiscount;
 	@NotNull(message = "{idObjectDiscount.not.null}")
+	@ApiModelProperty(notes = "Id discount", example = "000acde0-4d72-4049-9705-0eb5ce87e2fa", required = true)
 	private UUID idObjectDiscount;
 	@NotNull(message = "{percentageDiscount.not.null}")
+	@ApiModelProperty(notes = "Percentage discount", example = "0.1", required = true)
 	private Double percentageDiscount;
 	@DateTimeFormat(pattern = Conf.dateFormat)
+	@ApiModelProperty(notes = "Expiration date", example = "2024-09-30", required = true)
 	private LocalDate expiration;
 
 }
